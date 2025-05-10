996abeec3d028aca25e2d2ce80701e3bf18699fd
agamstawn <setiawanagam@gmail.com>
2025-05-11 00:57:42 +0700

    define association between user and sleep time record models

--- app/models/sleep_time_record.rb
+++ app/models/sleep_time_record.rb
@@ -1,2 +1,15 @@
 class SleepTimeRecord < ApplicationRecord
+  belongs_to :user
+
+  validates :sleep_time, presence: true
+  validate :wake_time_check
+
+  private
+
+  def wake_time_check
+    if wake_time.present? && wake_time <= sleep_time
+      errors.add(:wake_time, "must be after sleep time")
+    end
+  end
+
 end
